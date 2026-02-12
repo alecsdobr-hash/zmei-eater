918cdc47c5d2904de69d51d960629ba84d7cbc82
Alexandr <bananakuki@yandex.ru>
2026-02-12 23:14:35 +0300

    изменена скорость на более медленную

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ function movefood() {
         if (foodRight > closer) {
             openMounth.style.opacity = "1";
         }
-        food.style.left = currentLeft + (window.innerWidth * 0.08) + "px";
+        food.style.left = currentLeft + (window.innerWidth * 0.005) + "px";
     } else {
         setTimeout(function() {
         onfoodCome();
